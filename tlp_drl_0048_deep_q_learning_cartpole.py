50555aadc15491495370e2e77751360a3d38c31b
Michael James <mikeyj777@gmail.com>
2024-07-31 22:21:07 -0400

    48 - entered link to colab

---
+++ tlp_drl_0048_deep_q_learning_cartpole.py
@@ -0,0 +1,3 @@
+# due to issues with running tensorflow on this machine, this lesson was performed on google colab
+# link - https://github.com/lazyprogrammer/machine_learning_examples/blob/master/rl2/cartpole/dqn_tf.py
+# under mike@mylopa.app account
